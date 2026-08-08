a2f08df6dd407e96c4e2fb44a6ada5d136bf3eca
chyonma <yushaibnraihan@gmail.com>
2026-08-08 17:06:51 +0530

    Initialize Mnemos updating\

--- app/src-tauri/src/core/mod.rs
+++ app/src-tauri/src/core/mod.rs
@@ -1,3 +1,4 @@
+#[derive (Debug, Clone)]
 pub struct Installation{
     pub id: String,
     pub executable_path: String,
@@ -8,7 +9,30 @@ pub struct Installation{
     pub steam_app_id: Option<String>,
     pub manually_linked: bool,
 }
+impl Installation{
+    pub fn new(
+        executable_path: String,
+        executable_name: String,
+        install_directory: String,
+        display_name: String,
+    ) -> Self {
+        Self{ 
+            id: uuid::Uuid::new_v4().to_string(),
+            executable_path,
+            executable_name,
+            install_directory,
+            display_name,
+            known_launcher: None,
+            steam_app_id: None,
+            manually_linked: false,
 
+      
+        }
+    }
+
+}
+
+#[derive (Debug, Clone)]
 pub struct Session{
     pub id: String,
     pub installation_id: String,
