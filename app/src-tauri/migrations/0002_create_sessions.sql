6ab074c056af61690c933d46e0d812762118c51d
chyonma <yushaibnraihan@gmail.com>
2026-08-12 18:00:25 +0530

    migration fix

--- app/src-tauri/migrations/0002_create_sessions.sql
+++ app/src-tauri/migrations/0002_create_sessions.sql
@@ -2,7 +2,7 @@ CREATE TABLE sessions (
     if TEXT PRIMARY KEY NOT NULL,
     installation_id TEXT NOT NULL,
     started_at TEXT NOT NULL,
-    ended-at TEXT,
+    ended_at TEXT,
     duration_seconds INTEGER,
     manually_edited BOOLEAN NOT NULL DEFAULT 0,
     FOREIGN KEY (installation_id) REFERENCES installations(id)
