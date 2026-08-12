55fcbc68ee8ec98891e49fb60085332b59cbbb6c
chyonma <yushaibnraihan@gmail.com>
2026-08-12 17:16:20 +0530

    registering commands

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -3,19 +3,20 @@ mod storage;
 mod bridge;
 
 use sqlx::SqlitePool;
-pub struct AppState {
 
-  pub db: SqlitePool,
+pub struct AppState {
+    pub db: SqlitePool,
 }
 
 #[tokio::main]
-async fn main(){
-  let pool = storage::connect("sqlite://mnemos.db?mode=rwc")
-      .await
-      .expect("failed to connect to database");
+async fn main() {
+    let pool = storage::connect("sqlite://mnemos.db?mode=rwc")
+        .await
+        .expect("failed to connect to database");
 
-      tauri::Builder::default()
-      .manage(AppState{ db: pool})
-      .run(tauri::generate_context!())
-      .expect("error while running tauri application");
+    tauri::Builder::default()
+        .manage(AppState { db: pool })
+        .invoke_handler(tauri::generate_handler![bridge::get_installations])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
