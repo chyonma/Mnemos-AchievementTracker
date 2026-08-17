84b61d74eb1a36619425e82cbb6de4c778e4af12
chyonma <yushaibnraihan@gmail.com>
2026-08-17 09:48:09 +0530

    extending game detector

--- app/src-tauri/src/detection/mod.rs
+++ app/src-tauri/src/detection/mod.rs
@@ -1,6 +1,6 @@
 pub trait GameDetector {
     fn get_running_processes(&self) -> Vec<String>;
-    fn discover_installations(&self) -> Vec<crate::core::installation>
+    fn discover_installations(&self) -> Vec<crate::core::Installation>
 }
 
 pub mod windows;
